3053a607c59eb9b11da4b7dc09f062d26c784065
DESKTOP-JTDD7A9\vheh0 <vheh0901@gmail.com>
2022-10-09 00:06:38 +0900

    1.4 Profile에서 LogOut 구현

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,8 @@ import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom" 
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation.js";
+import Profile from "../routes/Profile"
 
 
 export default function AppRouter({ isLoggedIn }){
@@ -9,10 +11,12 @@ export default function AppRouter({ isLoggedIn }){
 
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? 
                 <>
                 <Route exact path="/" element={<Home />}></Route> 
+                <Route exact path="/profile" element={<Profile />}></Route> 
                 </>
                 : 
                 <Route excact path="/" element={ <Auth/>}></Route>}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,17 @@
+import { signOut } from "firebase/auth"
 import React from "react"
+import { authService } from "../myBase"
+import { useNavigate } from "react-router-dom"
 
 export default function Profile(){
-    return <span>Profile</span>
+    const history = useNavigate();
+    const onLogOutClick = () =>{
+        signOut(authService);
+        history("/")
+    } 
+    return(
+        <>
+        <button onClick={onLogOutClick}> Log Out</button>
+        </>
+    )
 }
